bb74c73c92ab7c891a036aee7fc2772b5ed67770
Bugen Zhao <i@bugenzhao.com>
2021-03-18 18:44:51 +0800

    flutter: add ci & Linux desktop support (#6)
    * core: add ci and update widget test
    
    * core: fix xcode build
    
    * core: update ci `build and deploy`
    
    * core: update ci
    
    * ci: update ci for linux desktop
    
    * android: update organization
    
    * linux: add support
    
    * core: fix ci for linux desktop; add artifact uploading

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,9 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:io';
-
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kanbasu/main.dart';
 
