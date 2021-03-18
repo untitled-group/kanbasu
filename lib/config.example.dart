5f05354aa9b608d4088deb58f8ede0218f0182a3
Alex Chi <iskyzh@gmail.com>
2021-03-18 16:49:06 +0800

    api: add buffer API for courses (#4)
    * api: add buffer API for courses
    
    * add docs

--- lib/config.example.dart
+++ lib/config.example.dart
@@ -1,4 +1,7 @@
+/// Canvas API key (For test use, will be removed later)
 const CANVAS_API_KEY = "xxxx";
-// use oc.sjtu.edu.cn on mobile
-// use localhost:5000 in web with `oc_server.py` started
+
+/// Canvas API Endpoint (For test use, will be removed later)
+/// * use oc.sjtu.edu.cn on mobile
+/// * use localhost:5000 in web with `oc_server.py` started
 const CANVAS_API_ENDPOINT = "http://127.0.0.1:5000/api/v1";
