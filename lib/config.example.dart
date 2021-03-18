465cbb50379c1c13a1dd7cb3d8889a2c165c0444
Alex Chi <iskyzh@gmail.com>
2021-03-18 16:37:45 +0800

    change endpoint

--- lib/config.example.dart
+++ lib/config.example.dart
@@ -1,4 +1,4 @@
 const CANVAS_API_KEY = "xxxx";
 // use oc.sjtu.edu.cn on Mobile and Desktop
 // use localhost:5000 in Web with `oc_server.py` started
-const CANVAS_API_ENDPOINT = "http://127.0.0.1:5000/api/v1";
+const CANVAS_API_ENDPOINT = "https://oc.sjtu.edu.cn/api/v1";
