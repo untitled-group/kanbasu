7446b9442a09014dd4fb94b535ca8ed418a58566
Alex Chi <iskyzh@gmail.com>
2021-03-18 16:50:36 +0800

    flutter: add macOS support (#5)

--- lib/config.example.dart
+++ lib/config.example.dart
@@ -2,6 +2,6 @@
 const CANVAS_API_KEY = "xxxx";
 
 /// Canvas API Endpoint (For test use, will be removed later)
-/// * use oc.sjtu.edu.cn on mobile
-/// * use localhost:5000 in web with `oc_server.py` started
-const CANVAS_API_ENDPOINT = "http://127.0.0.1:5000/api/v1";
+// use oc.sjtu.edu.cn on Mobile and Desktop
+// use localhost:5000 in Web with `oc_server.py` started
+const CANVAS_API_ENDPOINT = "https://oc.sjtu.edu.cn/api/v1";
