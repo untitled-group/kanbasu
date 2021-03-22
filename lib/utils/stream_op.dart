12f8f4e19742ee6d07f8c44032cc79abd1fe8533
Alex Chi <iskyzh@gmail.com>
2021-03-22 14:52:34 +0800

    minor refactor

--- lib/utils/stream_op.dart
+++ lib/utils/stream_op.dart
@@ -16,8 +16,8 @@ Stream<T> yieldLast<T>(Stream<T> stream) {
 Future<Stream<T>> Function(ReplayStream<T>) waitFor<T>(int N) {
   return (stream) async {
     // Prevent stream from being canceled
-    unawaited(stream.length.then((value) => null));
-    // Wait for at least 20 elements
+    stream.listen(null);
+    // Wait for at least `N` elements
     await stream.take(N).length;
     return stream;
   };
