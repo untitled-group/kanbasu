2e7b6b4f666923a3565385df622436aa25d91267
Alex Chi <iskyzh@gmail.com>
2021-04-01 11:12:58 +0800

    resolver: add available items

--- lib/resolver/resolver.dart
+++ lib/resolver/resolver.dart
@@ -1,5 +1,8 @@
 import 'package:kanbasu/buffer_api/canvas.dart';
+import 'package:kanbasu/models/assignment.dart';
 import 'package:kanbasu/models/course.dart';
+import 'package:kanbasu/models/file.dart';
+import 'package:kanbasu/models/module.dart';
 
 class ResolverProgress {
   final double percent;
@@ -16,6 +19,11 @@ class Resolver {
 
   Resolver(this._api);
 
+  ResolverProgress ofTotal(ResolverProgress progress, int of, int total) =>
+      ResolverProgress(
+          percent: progress.percent / total + of / total,
+          message: progress.message);
+
   /// Resolve all objects.
   Stream<ResolverProgress> resolve() async* {
     final courses = await _api.getCourses().last;
@@ -24,6 +32,51 @@ class Resolver {
     }
   }
 
-  /// Resolve `List<Course>`.
-  Stream<ResolverProgress> resolveCourses(List<Course> courses) async* {}
+  /// Resolve List of [Course].
+  Stream<ResolverProgress> resolveCourses(List<Course> courses) async* {
+    final length = courses.length;
+    var index = 0;
+    for (final course in courses) {
+      await for (final progress in resolveCourse(course)) {
+        yield ofTotal(progress, index, length);
+      }
+      index += 1;
+    }
+  }
+
+  /// Resolve [Course].
+  Stream<ResolverProgress> resolveCourse(Course course) async* {
+    final total = 3;
+
+    final files = await _api.getFiles(course.id).last;
+    await for (final progress in resolveFiles(files)) {
+      yield ofTotal(progress, 0, total);
+    }
+
+    final assignments = await _api.getAssignments(course.id).last;
+    await for (final progress in resolveAssignments(assignments)) {
+      yield ofTotal(progress, 1, total);
+    }
+
+    final modules = await _api.getModules(course.id).last;
+    await for (final progress in resolveModules(modules)) {
+      yield ofTotal(progress, 2, total);
+    }
+  }
+
+  /// Resolve stream of [File].
+  Stream<ResolverProgress> resolveFiles(Stream<File> files) async* {
+    await files.handleError((_) {}).toList();
+  }
+
+  /// Resolve stream of [Assignment].
+  Stream<ResolverProgress> resolveAssignments(
+      Stream<Assignment> assignments) async* {
+    await assignments.handleError((_) {}).toList();
+  }
+
+  /// Resolve stream of [Module].
+  Stream<ResolverProgress> resolveModules(Stream<Module> modules) async* {
+    await modules.handleError((_) {}).toList();
+  }
 }
---
+++ test/resolver_test.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:kanbasu/buffer_api/canvas.dart';
+import 'package:kanbasu/buffer_api/kvstore.dart';
+import 'package:kanbasu/resolver/resolver.dart';
+import 'package:test/test.dart';
+import 'package:dio/dio.dart';
+import 'package:kanbasu/rest_api/canvas.dart';
+import './mocks/mock_adapter.dart';
+
+void main() {
+  KvStore.initFfi();
+
+  group('Resolver', () {
+    final dio = Dio();
+    dio.httpClientAdapter = MockAdapter();
+    final restClient = CanvasRestClient(dio, baseUrl: MockAdapter.mockBase);
+    final api = CanvasBufferClient(restClient, null);
+
+    test('should be able to resolve all items', () async {
+      final resolver = Resolver(api);
+      await resolver.resolve().toList();
+    });
+  });
+}
