6f71f95de0c6ea773ffb9b6963f8f68b458acb9c
BugenZhao <i@bugenzhao.com>
2021-04-06 09:50:04 +0800

    add stream widget based files title widget

--- lib/screens/files.dart
+++ lib/screens/files.dart
@@ -2,12 +2,37 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:kanbasu/models/course.dart';
 import 'package:kanbasu/models/file.dart';
 import 'package:kanbasu/screens/list_screen.dart';
 import 'package:kanbasu/models/model.dart';
 import 'package:kanbasu/widgets/link.dart';
+import 'package:kanbasu/widgets/stream.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
+
+class _FilesTitle extends StreamWidget<Course?> {
+  final int courseId;
+
+  _FilesTitle(this.courseId);
+
+  @override
+  Widget buildWidget(Course? data) => Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: data == null
+          ? [
+              Text('tabs.file'.tr()),
+            ]
+          : [
+              Text('tabs.file'.tr(), style: TextStyle(fontSize: 20.0)),
+              Text(data.name, style: TextStyle(fontSize: 12.0)),
+            ]);
+
+  @override
+  Stream<Course?> getStream() =>
+      Provider.of<Model>(useContext()).canvas.getCourse(courseId);
+}
 
 class FilesScreen extends ListScreen<File> {
   final int courseId;
@@ -19,7 +44,7 @@ class FilesScreen extends ListScreen<File> {
       Provider.of<Model>(useContext()).canvas.getFiles(courseId);
 
   @override
-  Widget getTitle(s) => Text('tabs.file'.tr());
+  Widget getTitle(s) => useMemoized(() => _FilesTitle(courseId));
 
   @override
   Widget buildItem(File item) => Link(
