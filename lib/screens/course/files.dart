24da7b4806f37598b49f42e793375b8b9d8f8f05
BugenZhao <i@bugenzhao.com>
2021-05-06 15:10:46 +0800

    extract file widget, make only the icon pressable

--- lib/screens/course/files.dart
+++ lib/screens/course/files.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:kanbasu/models/file.dart';
 import 'package:kanbasu/models/model.dart';
-import 'package:kanbasu/models/resolver_model.dart';
 import 'package:kanbasu/widgets/common/refreshable_stream_list.dart';
+import 'package:kanbasu/widgets/file.dart';
 import 'package:provider/provider.dart';
 
 // ignore: unused_element
@@ -31,78 +31,6 @@ import 'package:provider/provider.dart';
 //       Provider.of<Model>(context).canvas.getCourse(courseId);
 // }
 
-enum FileStatus { Downloaded, Downloading, Remote }
-
-class _File extends HookWidget {
-  final File _item;
-
-  _File(this._item);
-
-  @override
-  Widget build(BuildContext context) {
-    final fileStatus = useState<FileStatus>(FileStatus.Remote);
-    final theme = Provider.of<Model>(context).theme;
-    final resolverModel = Provider.of<ResolverModel>(context);
-    final notifier = useValueListenable(resolverModel.getNotifierFor(_item.id));
-
-    useEffect(() {
-      resolverModel.fileResolver.getDownloadedFile(_item).then((value) {
-        if (value != null) {
-          fileStatus.value = FileStatus.Downloaded;
-        }
-      });
-    }, [notifier]);
-
-    final Widget statusWidget;
-
-    if (fileStatus.value == FileStatus.Downloaded) {
-      // downloaded
-      statusWidget = Icon(Icons.cloud_done_outlined);
-    } else if (resolverModel.isFileDownloading[_item.id] == true) {
-      // downloading
-      final percent = resolverModel.fileResolveProgress[_item.id]?.percent;
-      if (percent != null) {
-        // transferring
-        statusWidget = SizedBox(
-          width: 24,
-          height: 24,
-          child: CircularProgressIndicator(value: percent),
-        );
-      } else {
-        // queueing
-        statusWidget = Icon(Icons.cloud_queue_outlined);
-      }
-    } else {
-      // on cloud
-      statusWidget = Icon(
-        Icons.cloud_download_outlined,
-        color: theme.tertiaryText,
-      );
-    }
-
-    return InkWell(
-      onTap: () => resolverModel.requestDownload(_item),
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Text(_item.displayName)],
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: statusWidget,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class _FilesView extends RefreshableStreamListWidget<File> {
   final int courseId;
 
@@ -116,7 +44,7 @@ class _FilesView extends RefreshableStreamListWidget<File> {
       Provider.of<Model>(context).canvas.getFiles(courseId);
 
   @override
-  Widget buildItem(context, File item) => _File(item);
+  Widget buildItem(context, File item) => FileWidget(item);
 }
 
 class CourseFilesScreen extends HookWidget {
