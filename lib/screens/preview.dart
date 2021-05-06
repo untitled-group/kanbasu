076f6f2a1504f18cd1585fc26c492c6451078994
BugenZhao <i@bugenzhao.com>
2021-05-06 16:49:29 +0800

    add 2 preview providers

--- lib/screens/preview.dart
+++ lib/screens/preview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:kanbasu/models/local_file.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:kanbasu/models/resolver_model.dart';
@@ -6,21 +7,76 @@ import 'package:kanbasu/widgets/common/future.dart';
 import 'package:kanbasu/widgets/loading.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
 import 'package:provider/provider.dart';
+import 'package:pdf_render/pdf_render_widgets.dart';
 
-class PreviewScreen extends FutureWidget<LocalFile?> {
+class PreviewScreen extends StatelessWidget {
   final String fileId;
 
   PreviewScreen({required this.fileId});
 
   @override
+  Widget build(BuildContext context) {
+    return PdfRenderPreviewScreen(fileId: fileId);
+  }
+}
+
+class PdfRenderPreviewScreen extends FutureWidget<LocalFile?> {
+  final String fileId;
+
+  PdfRenderPreviewScreen({required this.fileId});
+
+  @override
   Widget buildWidget(BuildContext context, LocalFile? data) {
-    final title = data?.path.split('/').last ?? 'File';
+    final title = data?.path.split('/').last ?? 'title.preview'.tr();
+
     final body = data == null
         ? LoadingWidget(isMore: false)
-        : PdfView(
-            controller: PdfController(
+        : PdfViewer.openFile(
+            data.path,
+            viewerController: PdfViewerController(),
+          );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: body,
+    );
+  }
+
+  @override
+  List<Future<LocalFile?>> getFutures(BuildContext context) => [
+        Provider.of<ResolverModel>(context)
+            .fileResolver
+            .getDownloadedFileById(fileId),
+      ];
+}
+
+class NativePdfViewPreviewScreen extends FutureWidget<LocalFile?> {
+  final String fileId;
+
+  NativePdfViewPreviewScreen({required this.fileId});
+
+  @override
+  Widget buildWidget(BuildContext context, LocalFile? data) {
+    final title = data?.path.split('/').last ?? 'title.preview'.tr();
+
+    final horizontal = useState(false);
+    final controller = useMemoized(
+      () => data == null
+          ? null
+          : PdfController(
               document: PdfDocument.openFile(data.path),
             ),
+      [data],
+    ); // bug of `package:native_pdf_view`
+
+    final body = controller == null
+        ? LoadingWidget(isMore: false)
+        : PdfView(
+            controller: controller,
+            scrollDirection: horizontal.value ? Axis.horizontal : Axis.vertical,
+            pageSnapping: false,
           );
 
     return Scaffold(
@@ -28,9 +84,14 @@ class PreviewScreen extends FutureWidget<LocalFile?> {
         title: Text(title),
         actions: [
           IconButton(
-              icon: Icon(Icons.settings),
-              tooltip: 'title.settings'.tr(),
-              onPressed: () {})
+            icon: Icon(horizontal.value
+                ? Icons.swap_horiz_outlined
+                : Icons.swap_vert_outlined),
+            tooltip: 'preview.change_dir'.tr(),
+            onPressed: () {
+              horizontal.value = !horizontal.value;
+            },
+          )
         ],
       ),
       body: body,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import native_pdf_renderer
 import path_provider_macos
+import pdf_render
 import shared_preferences_macos
 import sqflite
 import url_launcher_macos
@@ -15,6 +16,7 @@ import wakelock_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   NativePdfRendererPlugin.register(with: registry.registrar(forPlugin: "NativePdfRendererPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
