e06f790ad5b8cb9bf038fa0d6489cbe28f564c88
Yimin Zhao <49093593+doctormin@users.noreply.github.com>
2021-05-07 08:25:16 +0800

    ui: add page screen (#100)
    * ui: page screen: finish page widget
    
    * ui: finish page screen
    
    * ui: clean the code
    
    * ui: locale page screen
    
    * refine pages widget
    
    Co-authored-by: Alex Chi <iskyzh@gmail.com>

---
+++ lib/screens/course/pages.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:kanbasu/models/model.dart';
+import 'package:kanbasu/models/page.dart' as p;
+import 'package:kanbasu/widgets/page.dart';
+import 'package:kanbasu/widgets/common/refreshable_stream_list.dart';
+import 'package:provider/provider.dart';
+
+class CoursePagesScreen extends RefreshableStreamListWidget<p.Page> {
+  final int courseId;
+
+  CoursePagesScreen(this.courseId);
+  @override
+  List<Stream<p.Page>> getStreams(context) =>
+      Provider.of<Model>(context).canvas.getPages(courseId);
+
+  @override
+  Widget buildItem(context, p.Page item) {
+    return InkWell(
+      onTap: () => showModalBottomSheet(
+        context: context,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+        ),
+        isScrollControlled: true,
+        builder: (context) {
+          return ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height * 0.3,
+              maxHeight: MediaQuery.of(context).size.height * 0.9,
+            ),
+            child: PageContentWidget(courseId, item.pageId),
+          );
+        },
+      ),
+      child: PageWidget(item),
+    );
+  }
+}
---
+++ lib/widgets/page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:kanbasu/models/page.dart' as p;
+import 'package:kanbasu/models/model.dart';
+import 'package:kanbasu/widgets/common/future.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class PageWidget extends StatelessWidget {
+  final p.Page item;
+  PageWidget(this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<Model>(context).theme;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text.rich(
+                  TextSpan(
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: theme.text,
+                      ),
+                      children: [
+                        TextSpan(text: item.title.trim()),
+                      ]),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PageContentWidget extends FutureWidget<p.Page?> {
+  final int pageId;
+  final int courseId;
+
+  PageContentWidget(this.courseId, this.pageId);
+
+  @override
+  List<Future<p.Page?>> getFutures(BuildContext context) =>
+      Provider.of<Model>(context).canvas.getPage(courseId, pageId);
+
+  @override
+  Widget buildWidget(BuildContext context, p.Page? data) {
+    String htmlData;
+    if (data == null) {
+      htmlData = '<h3> ${'page.no_details'.tr()} </h3>';
+    } else {
+      htmlData = data.body ?? '<h3> ${'page.no_details'.tr()} </h3>';
+    }
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        if (data != null) PageWidget(data),
+        Html(data: htmlData),
+      ],
+    );
+  }
+}
